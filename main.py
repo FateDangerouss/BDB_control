912db28db060ca4f1db9a03aa18e911d560f1322
Sherry <chenji15sss@gmail.com>
2021-03-01 23:59:29 +0800

    23:59 03/03/2021

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
-import gpio
+from smbus import SMBus
+from PCA9685 import PWM
 import time
--- ultrasonic.py
+++ ultrasonic.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import time
 
 class Ultrasonic:
     def __init__(self, TRIG, ECHO):
@@ -6,24 +7,25 @@ class Ultrasonic:
         GPIO.setmode(GPIO.BCM)
         self.TRIG = TRIG
         self.ECHO = ECHO
+        self.setup()
     
     def setup(self):
         GPIO.setup(self.TRIG, GPIO.OUT)
         GPIO.setup(self.ECHO, GPIO.IN)
         
     def distance(self):
-        GPIO.output(TRIG, GPIO.LOW)
+        GPIO.output(self.TRIG, GPIO.LOW)
         time.sleep(0.000002)
 
-        GPIO.output(TRIG, GPIO.HIGH)
+        GPIO.output(self.TRIG, GPIO.HIGH)
         time.sleep(0.00001)
-        GPIO.output(TRIG, GPIO.LOW)
+        GPIO.output(self.TRIG, GPIO.LOW)
 
-        while GPIO.input(ECHO) == 0:
+        while GPIO.input(self.ECHO) == 0:
             pass
         time1 = time.time()
 
-        while GPIO.input(ECHO) == 1:
+        while GPIO.input(self.ECHO) == 1:
             pass
         time2 = time.time()
 
