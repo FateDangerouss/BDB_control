5a4abd67e43484a9e1b8f37b09a98c23c4511df4
Sherry <chenji15sss@gmail.com>
2021-03-07 16:48:04 +0800

    16:48 03/07/2021

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import robotic_arm
 import time
 import gpio
 import threading
-import multiprocessing as mp
 
-detect = gpio.myGPIO([17, 'OUT', 18, 'IN', 27, 'IN', 22, 'IN', 23, 'IN', 24, 'IN', 25, 'IN', 4, 'IN', 5, 'IN', 6, 'OUT'])
+pm_1 = 3
+pm_2 = 4
+pm_3 = 5
+pm_4 = 6
+
+IO = gpio.myGPIO([17, 'OUT', 18, 'IN', 27, 'IN', 22, 'IN', 23, 'IN', 24, 'IN', 25, 'IN', 4, 'IN', 5, 'IN', 6, 'OUT'])
 #17: 工作状态信息输出
 #18: 箱1输入信息
 #27: 箱2输入信息
@@ -16,22 +20,39 @@ detect = gpio.myGPIO([17, 'OUT', 18, 'IN', 27, 'IN', 22, 'IN', 23, 'IN', 24, 'IN
 #5:  箱4集满信息
 #6:  继电器控制输出
 
+ra_1 = robotic_arm.RoboticArm(0)
+ra_2 = robotic_arm.RoboticArm(4)
+ra_3 = robotic_arm.RoboticArm(8)
 
-ra1 = robotic_arm.RoboticArm(0)
-ra2 = robotic_arm.RoboticArm(4)
-ra3 = robotic_arm.RoboticArm(8)
 
-time.sleep(2)
-def ra1run():
-    ra1.run()
-def ra2run():
-    ra2.run()
-def ra3run():
-    ra3.run()
-if __name__ == '__main__':
-    list = []
-    list.append(mp.Process(target=ra1run()))
-    list.append(mp.Process(target=ra2run()))
-    list.append(mp.Process(target=ra3run()))
-    for t in list:
-        t.start()
+while True:
+    if IO.input(18) == 1:
+        IO.output(17, 'HIGH')
+        IO.output(6, 'HIGH')
+        time.sleep(pm_1)
+        IO.output(6, 'LOW')
+        ra_1.run()
+        IO.output(17, 'LOW')
+    elif IO.input(27) == 1:
+        IO.output(17, 'HIGH')
+        IO.output(6, 'HIGH')
+        time.sleep(pm_2)
+        IO.output(6, 'LOW')
+        ra_2.run()
+        IO.output(17, 'LOW')
+    elif IO.input(22) == 1:
+        IO.output(17, 'HIGH')
+        IO.output(6, 'HIGH')
+        time.sleep(pm_3)
+        IO.output(6, 'LOW')
+        ra_3.run()
+        IO.output(17, 'LOW')
+    elif IO.input(23) == 1:
+        IO.output(17, 'HIGH')
+        IO.output(6, 'HIGH')
+        time.sleep(pm_4)
+        IO.output(6, 'LOW')
+        IO.output(17, 'LOW')
+        print("Fuck                                                                                           ")
+    else:
+        time.sleep(0.1)
